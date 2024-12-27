a7518e7a2e3186be48fa314a39b1fbf1c8fb23c6
Rodivan Peterson <peterodi57@gmail.com>
2024-12-27 15:19:15 -0300

    create section-header component

--- src/app/components/section-header/section-header.component.ts
+++ src/app/components/section-header/section-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-section-header',
@@ -11,5 +11,8 @@ export class SectionHeaderComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  @Input() sectionName: string = '';
+  @Input() hideButtonLabel: boolean = false;
 
 }
