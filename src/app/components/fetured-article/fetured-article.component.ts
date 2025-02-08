165e1b7c9ca9a9f1b08afd20fc97cd5bf7864510
Rodivan Peterson <peterodi57@gmail.com>
2025-02-08 19:13:49 -0300

    create the fetured-article component

--- src/app/components/fetured-article/fetured-article.component.ts
+++ src/app/components/fetured-article/fetured-article.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, Input } from '@angular/core';
 
 @Component({
   selector: 'app-fetured-article',
@@ -7,6 +7,19 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class FeturedArticleComponent implements OnInit {
+
+  @Input() articleTitle: string = '';
+  @Input() articleResume: string = '';
+  @Input() authorName: string = '';
+  @Input() authorProfilePicture: string = '';
+  @Input() publisedAt: string = '';
+  @Input() imageArticle: string = '';
+  @Input() category: string = '';
+  @Input() readingTime: string = '';
+  @Input() articleURL: string = '#';
+  showDescription: boolean = true;
+  isSmallCard: boolean = false;
+  imagePosition: string = 'left';
 
   constructor() { }
 
